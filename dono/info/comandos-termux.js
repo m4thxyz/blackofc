ff83b6d554beb484703c5769fd703867e2155992
matheuzinho <122645154+m4thxyz@users.noreply.github.com>
2024-06-20 23:11:21 -0300

    Add files via upload

--- dono/info/comandos-termux.js
+++ dono/info/comandos-termux.js
@@ -6,33 +6,29 @@ https://www.mediafire.com/file/0npdmv51pnttps0/com.termux_0.119.1-119_minAPI21(a
 *_Após instalado, configure ele com os comandos abaixo:_*
 
 1.
-termux-change-repo 
-
-Confirma, marca a terceira caixinha e confirma e prossegue > 
-
-2.
-apt-get upgrade
-
-Vai precisar digitar y e confirmar toda vez que pedir.
-
-3.
-apt-get update
-
-Vai precisar digitar y e confirmar toda vez que pedir.
-
-4.
 pkg upgrade -y && pkg update -y && pkg install nodejs -y && pkg install nodejs-lts -y && pkg install ffmpeg -y && pkg install wget -y && pkg install git -y
 
-5.
+Vai precisar digitar y e confirmar toda vez que pedir.
+
+2.
 termux-setup-storage
 
-E permite.
+Espera um pouco, e permite.
 
-*_Após configurado, vamos pra instalação (caso você não tenha o arquivo, instale o mesmo no site do bot: https://blackmd.online/docs ):_*
+3.
+cd /sdcard && git clone https://github.com/m4thxyz/blackofc
 
-6.
-cd /sdcard/Download/BLACKOFC
-7.
+4.
+cd blackofc && sh blackmd.sh
+
+Daí basta colocar o número e conectar no whatsapp.
+
+
+
+_Caso o bot desligue do termux e termine a sessão, basta usar o seguinte comando:_
+cd /sdcard/blackofc && sh blackmd.sh
+
+_Ou se a sessão permanecer aberta, basta usar:_
 sh blackmd.sh
 `
 }
